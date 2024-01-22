b0767a186e2b47149a3dad04cb804160e40fbc1c
oxxn <oxxn@oxxn.com>
2024-01-22 01:51:52 -0500

    Working now

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ fn currency_input_markup(btc_value: &str, usd_value: &str) -> Markup {
                 id="btc"
                 name="amount"
                 type="text"
-                hx-trigger="input, keyup"
+                hx-trigger="input, keyup, every 3s"
                 hx-get="/convert_currency"
                 hx-vals=r#"{"currency": "BTC"}"#
                 hx-target="#currency-converter"
@@ -109,7 +109,8 @@ async fn convert_currency(
     Html(currency_input_markup(&btc_value, &usd_value).into_string())
 }
 
-async fn root() -> Html<String> {
+async fn root(state: State<Arc<Mutex<AppState>>>) -> Html<String> {
+    let exchange_rate = state.lock().unwrap().exchange_rate;
     let markup = html! {
         (DOCTYPE)
         head {
@@ -138,7 +139,7 @@ async fn root() -> Html<String> {
             }
             main class="container" {
                 div id="currency-converter" {
-                    (currency_input_markup("1", "40,300"))
+                    (currency_input_markup("1", &exchange_rate.to_string()))
                 }
             }
         }
@@ -148,21 +149,23 @@ async fn root() -> Html<String> {
 }
 
 async fn fetch_exchange_rate(state: Arc<Mutex<AppState>>) -> Result<(), reqwest::Error> {
-    let url = "https://api.coinbase.com/v2/exchange-rates?currency=BTC";
-    let client = reqwest::Client::new();
-    let res = client.get(url).send().await?;
+    let mut interval = tokio::time::interval(tokio::time::Duration::from_secs(3));
+    loop {
+        interval.tick().await;
+        let url = "https://api.coinbase.com/v2/exchange-rates?currency=BTC";
+        let client = reqwest::Client::new();
+        let res = client.get(url).send().await?;
 
-    let json: serde_json::Value = res.json().await?;
-    let rate = json["data"]["rates"]["USD"]
-        .as_str()
-        .unwrap_or("0")
-        .parse()
-        .unwrap_or(0.0);
+        let json: serde_json::Value = res.json().await?;
+        let rate = json["data"]["rates"]["USD"]
+            .as_str()
+            .unwrap_or("0")
+            .parse()
+            .unwrap_or(0.0);
 
-    let mut state = state.lock().unwrap();
-    state.exchange_rate = rate;
-
-    Ok(())
+        let mut state = state.lock().unwrap();
+        state.exchange_rate = rate;
+    }
 }
 
 #[tokio::main]
