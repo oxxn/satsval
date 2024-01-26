111a94c734d22bafcdad18f163223b262a5f48b7
oxxn <oxxn@oxxn.com>
2024-01-26 16:28:58 -0500

    Compile assets into exe to serve from memory.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
-use axum::{response::Html, routing::get, Router};
+use axum::{
+    body::Body,
+    http::{HeaderValue, Response},
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
 use maud::{html, DOCTYPE};
-use tower_http::services::ServeFile;
+use tokio::net::TcpListener;
 
 async fn root() -> Html<String> {
     let markup = html! {
@@ -40,19 +46,36 @@ async fn root() -> Html<String> {
     Html(markup.into_string())
 }
 
+async fn script_js() -> impl IntoResponse {
+    let content = include_str!("../static/script.js");
+    let body = Body::from(content);
+    let mut response = Response::new(body);
+    response.headers_mut().insert(
+        axum::http::header::CONTENT_TYPE,
+        HeaderValue::from_static("application/javascript"),
+    );
+    response
+}
+
+async fn geologica_regular_ttf() -> impl IntoResponse {
+    let content = include_bytes!("../static/Geologica-Regular.ttf");
+    let body = Body::from(content.as_ref());
+    let mut response = Response::new(body);
+    response.headers_mut().insert(
+        axum::http::header::CONTENT_TYPE,
+        HeaderValue::from_static("font/ttf"),
+    );
+    response
+}
+
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/", get(root))
-        .nest_service("/static/script.js", ServeFile::new("./static/script.js"))
-        .nest_service(
-            "/static/Geologica-Regular.ttf",
-            ServeFile::new("./static/Geologica-Regular.ttf"),
-        );
+        .route("/static/script.js", get(script_js))
+        .route("/static/Geologica-Regular.ttf", get(geologica_regular_ttf));
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-        .await
-        .unwrap();
+    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
     println!("Listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
