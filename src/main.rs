59bbe06a7ec5d99aa8bba9d71bff7341138cf98c
oxxn <oxxn@oxxn.com>
2024-01-28 02:47:26 -0500

    image/ico

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ async fn root() -> Html<String> {
                 meta name="viewport" content="width=device-width, initial-scale=1.0" {}
                 title { "SATSVAL" }
                 link rel="preload" href="/static/Geologica-Regular.ttf" as="font" crossorigin {}
-                link rel="icon" href="/favicon.ico" type="image/x-icon" {}
+                link rel="icon" href="/favicon.ico" type="image/ico" {}
                 style { r#"
                     @font-face {
                         font-family: 'Geologica';
